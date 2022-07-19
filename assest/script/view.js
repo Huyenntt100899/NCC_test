480a7a67b4a7935c533aea4c7c07fbf5d639acd4
Huyenntt100899 <nguyenhuyen10899@gmail.com>
2022-07-19 17:40:48 +0700

    test

--- assest/script/view.js
+++ assest/script/view.js
@@ -2,9 +2,9 @@
 const menu = document.getElementById("nav");
 const wrapper = document.getElementById("lorem");
 
-["Home", "Services", "News", "Blog", "Contact"].map((item)=>{
+["Home", "Services", "News", "Blog", "Contact"].map((item, index)=>{
     menu.innerHTML += `
-        <li class="nav-item">${item}</li>
+        <li class="nav-item ${index==0?'active':''} "}>${item}</li>
     `
 })
 
@@ -23,4 +23,13 @@ data.map((item)=>{
         </div>
     `
 })
+
+let menuItems = document.getElementsByClassName("nav-item");
+for (var i = 0; i < menuItems.length; i++) {
+    menuItems[i].addEventListener("click", function() {
+      var current = document.getElementsByClassName("active");
+      current[0].className = current[0].className.replace(" active", "");
+      this.className += " active";
+    });
+  }
 
